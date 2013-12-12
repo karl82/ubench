e2dc0e56e2d635a32711e964e0dd7341db89c2b9
Karel Rank <karel.rank@gmail.com>
2013-12-12 23:38:24 +0100

    Fix path to classes

--- bp.sh
+++ bp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-perf stat -d java -cp out/production/ubench cz.rank.ubench.bp.BranchPrediction1 > bp-1-perf.log 2>&1
-perf stat -d java -cp out/production/ubench cz.rank.ubench.bp.BranchPrediction2 > bp-2-perf.log 2>&1
+perf stat -d java -cp target/classes cz.rank.ubench.bp.BranchPrediction1 > bp-1-perf.log 2>&1
+perf stat -d java -cp target/classes cz.rank.ubench.bp.BranchPrediction2 > bp-2-perf.log 2>&1
